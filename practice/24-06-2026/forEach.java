7c2927b8593ad3307d9fd810c07adfe16d5bf787
Vishal <vishalbhilwariya@gmail.com>
2026-06-24 15:21:02 +0530

    Practice some java core problems

---
+++ practice/24-06-2026/forEach.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] nums = {1,2,3,4,5,6,7,8,9};
+		for(int num : nums){
+		    System.out.println(num);
+		}
+	}
+}
