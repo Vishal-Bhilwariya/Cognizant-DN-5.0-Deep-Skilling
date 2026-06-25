fe15537234f689a5065155cff51a9ceb74e4d7ae
Vishal <vishalbhilwariya@gmail.com>
2026-06-25 15:54:57 +0530

    Added notes of OOPs vs Procedural Programming

---
+++ Week_1/Module 1 - Design Patterns and Principles/1) OOPs/c) OOP vs Procedural Programming.java
@@ -0,0 +1,42 @@
+/*
+
+OOP vs Procedural Programming
+    Feature	            Procedural Programming	                            Object-Oriented Programming (OOP)
+    Approach	        Focuses on functions/procedures	                    Focuses on objects and classes
+    Data & Functions	Data and functions are separate	                    Data and methods are combined in objects (Encapsulation)
+    Code Reusability	Limited	                                            High (using inheritance, polymorphism, etc.)
+    Security	        Less secure (global data can be accessed easily)	More secure (data can be hidden using encapsulation)
+    Maintenance	        Difficult for large projects	                    Easier for large and complex projects
+    Real-World          Modeling	                                        Not based on real-world objects	Models real-world entities (Car, Student, Employee, etc.)
+    Examples	        C, Pascal	                                        Java, C++, Python, C#
+
+
+Procedural Programming (C) - Procedural Programming organizes a program into functions and follows a step-by-step approach. It is suitable for small to medium-sized applications.
+
+    Focus is on functions:
+        main()
+           |
+        login()
+           |
+        calculateSalary()
+           |
+        printReport()
+
+    Everything revolves around functions.
+
+Object-Oriented Programming (Java) - Object-Oriented Programming (OOP) organizes a program into classes and objects, making code more reusable, secure, and easier to maintain, especially for large applications.
+
+    Focus is on objects:
+
+        Student Object
+           |
+           |-- name
+           |-- rollNo
+           |-- marks
+           |
+           |-- study()
+           |-- attendClass()
+
+    Everything related to a student is kept together.
+
+*/
