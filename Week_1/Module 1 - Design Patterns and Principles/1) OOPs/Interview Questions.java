d6dde59c74929accbc7faeb338608a98b81a9077
Vishal <vishalbhilwariya@gmail.com>
2026-06-25 15:22:33 +0530

    Added Interview question of Object & Class

--- Week_1/Module 1 - Design Patterns and Principles/1) OOPs/Interview Questions.java
+++ Week_1/Module 1 - Design Patterns and Principles/1) OOPs/Interview Questions.java
@@ -20,4 +20,29 @@ Scenario-Based
 Q6. Your company is building a banking application with millions of lines of code. Which programming approach would you choose and why?
 Answer: I would choose Object-Oriented Programming (OOP) because it provides modularity, code reusability, data security through encapsulation, easier maintenance, and better scalability for large applications.
 
+Basic
+Q7. What is a class?
+Answer: A class is a blueprint or template that defines the properties (data) and behaviors (methods) of objects.
+
+Q8. What is an object?
+Answer: An object is a real instance of a class that contains actual data and can use the methods defined in the class.
+
+Q9. Can we create multiple objects from one class?
+Answer: Yes. One class can be used to create any number of objects.
+
+Intermediate
+Q10. Does a class occupy memory?
+Answer: The class definition itself does not store data for instances. Memory is allocated when objects are created.
+
+Q11. Why do we create classes?
+Answer: Classes help organize code, promote reusability, improve maintainability, and model real-world entities.
+
+Scenario-Based
+Q12. You are developing a library management system. Which classes might you create?
+Answer: Possible classes include:
+        Book
+        Student
+        Librarian
+        Library
+        IssueRecord
 */
