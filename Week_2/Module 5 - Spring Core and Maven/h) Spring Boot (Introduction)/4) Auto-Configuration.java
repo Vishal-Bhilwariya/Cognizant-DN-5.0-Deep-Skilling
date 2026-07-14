6008177b2c8cfd4beb9726830880c1ee5eda8761
Vishal Bhilwariya <vishalbhilwariya@gmail.com>
2026-07-14 22:43:35 +0530

    Auto Configuration

---
+++ Week_2/Module 5 - Spring Core and Maven/h) Spring Boot (Introduction)/4) Auto-Configuration.java
@@ -0,0 +1,31 @@
+/*
+
+    # Auto-Configuration :
+    - Auto-Configuration is a Spring Boot feature that automatically configures the application based on the dependencies available in the project.
+
+    Real-Life Analogy
+        Imagine checking into a hotel.
+
+     Traditional Spring:
+        You have to request separately:
+            Room
+            Wi-Fi
+            Towels
+            Breakfast
+
+     Spring Boot:
+        You book the room.
+        Everything is already prepared.
+
+    Role of @EnableAutoConfiguration
+    Spring Boot provides: @EnableAutoConfiguration
+
+    Its purpose is:  Automatically configure Spring Beans based on the project dependencies
+
+Advantages
+     Reduces manual configuration.
+     Saves development time.
+     Less boilerplate code.
+     Faster application setup.
+     Easy dependency-based configuration
+*/
