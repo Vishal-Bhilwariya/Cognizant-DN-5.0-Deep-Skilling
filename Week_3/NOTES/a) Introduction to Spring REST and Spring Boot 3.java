ebe04b85148701e33f44d980874ad27197931b9d
Vishal Bhilwariya <vishalbhilwariya@gmail.com>
2026-07-18 11:25:54 +0530

    Added notes of Introduction to Spring Rest and Spring Boot 3

---
+++ Week_3/NOTES/a) Introduction to Spring REST and Spring Boot 3.java
@@ -0,0 +1,218 @@
+/*
+
+    # Overview of RESTful Architecture : 
+     
+     REST - stands for Representational State Transfer.
+
+        It is an architectural style used to build communication between client applications and servers over the Internet using HTTP.
+        REST is not a language, not a framework, and not a protocol.
+        It is simply a set of architectural principles.
+
+        REST is a way of designing web applications so that different systems (mobile apps, websites, desktop applications, etc.) can communicate with each other easily.
+
+    # Why do we need REST?
+     - Before REST became popular, applications mainly used:
+        Remote Procedure Calls (RPC)
+        SOAP Web Services
+
+     Problems:
+        Complex
+        Heavy XML messages
+        Difficult to maintain
+        Slow
+        Tight coupling
+
+     REST solved these problems by introducing:
+        Simplicity
+        Lightweight communication
+        HTTP support
+        JSON
+        Scalability
+
+    # Real World Example
+     Imagine Swiggy.
+        When you open the app,
+            You request -> GET /restaurants
+            Server returns -> Restaurant List
+
+        When you place an order
+            POST /orders
+            Server creates order.
+
+        When you cancel
+            DELETE /orders/25
+
+     Everything happens using HTTP.
+    This is REST.
+
+
+    # REST Architecture :
+                        Internet
+
+                      HTTP Request
+            +------------------------------+
+            |          Client              |
+            |------------------------------|
+            | Browser                      |
+            | Mobile App                   |
+            | React App                    |
+            | Flutter App                  |
+            +------------------------------+
+                         |
+                         |
+                         V
+            +------------------------------+
+            |      REST API Server         |
+            |------------------------------|
+            | Spring Boot                  |
+            | Controllers                  |
+            | Services                     |
+            | Repository                   |
+            +------------------------------+
+                         |
+                         |
+                         V
+            +------------------------------+
+            |        Database              |
+            | MySQL / PostgreSQL           |
+            +------------------------------+
+
+    Internal Working
+
+     Step 1 : Client sends HTTP Request
+        GET /employees
+            ↓
+     Step 2 :  Spring Boot receives request
+            ↓
+     Step 3 : DispatcherServlet identifies matching controller
+            ↓
+     Step 4 : Controller calls Service
+            ↓
+     Step 5 : Service calls Repository
+            ↓
+     Step 6 : Repository fetches data
+            ↓
+     Step 7 : Response returned as JSON
+            ↓
+     Step 8 : Client receives JSON
+
+    # REST Constraints :
+    1. Client-Server - Client and Server are separate.
+     Example
+        React
+          ↓
+        Spring Boot
+          ↓
+        Database
+
+     Advantage - Independent development.
+
+    2. Stateless - Every request is independent.
+                    Server remembers nothing.
+     Example
+        Request 1
+            Login
+              ↓
+            Completed
+
+        Request 2
+            Get Employee
+                ↓
+            Must again send JWT Token
+
+        Server stores no client session.
+
+    3. Cacheable - Responses may be cached.
+     Example
+        GET Countries
+        No need to request every time.
+
+     Improves performance.
+
+    4. Uniform Interface- Every REST API follows similar rules.
+     Example
+        GET /users
+        POST /users
+        PUT /users/10
+        DELETE /users/10
+
+     Easy to understand.
+
+    5. Layered System - Client never knows whether request goes through:
+        Load Balancer
+            ↓
+        Gateway
+            ↓
+        Security
+            ↓
+        Application
+            ↓
+        Database
+
+    6. Code on Demand (Optional)
+        Server may send executable code.
+        Rarely used today.
+
+    # HTTP Methods
+        Method	Purpose
+        GET	    Read Data
+        POST	Create Data
+        PUT	    Update Entire Resource
+        PATCH	Partial Update
+        DELETE	Delete Resource
+
+    # URI Examples
+        GET /students
+        GET /students/10
+        POST /students
+        PUT /students/10
+        DELETE /students/10
+        
+    # HTTP Status Codes
+        Code	Meaning
+        200	     OK
+        201	     Created
+        204	     No Content
+        400	     Bad Request
+        401	     Unauthorized
+        403	     Forbidden
+        404	     Not Found
+        500	     Internal Server Error
+
+    # Advantages
+        Lightweight
+        Easy to understand
+        Platform independent
+        Supports JSON
+        Scalable
+        Stateless
+        Easy integration
+        High performance
+
+    # Introduction to Spring REST :
+    - Spring REST is the Spring Framework's support for building RESTful web services. In Spring Boot 3, you typically create REST APIs using Spring MVC (or Spring Web) with annotations such as @RestController.
+
+     Spring REST handles:
+        Receiving HTTP requests
+        Routing them to Java methods
+        Converting Java objects to JSON (serialization)
+        Converting JSON to Java objects (deserialization)
+        Sending HTTP responses
+        Why do we need Spring REST?
+
+     Without Spring:
+        You would manually parse HTTP requests.
+        You would manually generate JSON.
+        You would manually manage routing.
+
+     Spring REST automates these tasks, allowing developers to focus on business logic.
+
+    # Common Annotations
+        Name            	Description	                          Purpose	                Example
+        @RestController	    Marks a class as REST controller	Handles REST requests	@RestController
+        @RequestMapping	    Maps base URL	                    Groups endpoints	    @RequestMapping("/api")
+        @GetMapping	        Maps GET request	                Read data	            @GetMapping("/users")
+        @PostMapping	    Maps POST request	                Create data	            @PostMapping("/users")
+        @PutMapping	        Maps PUT request	                Update data	            @PutMapping("/users/{id}")
+        @DeleteMapping	    Maps DELETE request	                Delete data	            @DeleteMapping("/users/{id}")
+*/
