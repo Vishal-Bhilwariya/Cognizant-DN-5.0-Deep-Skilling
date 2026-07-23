9e6d432be4660bb39688d611168476ec6f671bbc
Vishal Bhilwariya <vishalbhilwariya@gmail.com>
2026-07-23 14:00:42 +0530

    Challenges of Microservices

---
+++ Module 8 - Microservices with Spring Boot 3 and Spring Cloud/c) Challenges of Microservices.java
@@ -0,0 +1,44 @@
+/*
+
+    # Complexity Management
+     - Complexity Management means handling the increased complexity that arises when one application is split into many independent microservices.
+
+     In a Monolithic application:
+        One Application
+        Everything is in one place.
+
+     In Microservices:
+        Login Service
+        Product Service
+        Order Service
+        Payment Service
+        Inventory Service
+        Notification Service
+        Review Service
+        Instead of managing 1 application, you now manage many applications.
+
+        This increases system complexity.
+
+    Real-Life Analogy
+     Shopping Mall
+        Managing one small shop is easy.
+        Managing an entire shopping mall with:
+            200 shops
+            Security
+            Electricity
+            Parking
+            Cleaning
+            Maintenance
+        is much more complex.
+
+        Microservices are similar.
+
+    Advantages (of Managing Complexity Well)
+        Better organization
+        Independent teams
+        Easier maintenance
+        Faster development
+
+    
+
+*/
