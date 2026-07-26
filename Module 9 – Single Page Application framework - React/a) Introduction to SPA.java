843771c7af68775c58b51367d8a5ed231e73632e
Vishal Bhilwariya <vishalbhilwariya@gmail.com>
2026-07-26 22:19:26 +0530

    Notes of WEEK 1 created by {Vishal}

--- Module 9 – Single Page Application framework - React/a) Introduction to SPA.java
+++
@@ -1,7 +0,0 @@
-/*
-
-    # A Single Page Application (SPA) is a web application that loads only one HTML page initially. After that, whenever the user performs any action (clicks a button, opens a profile, navigates to another page), only the required part of the page is updated instead of reloading the entire webpage.
-
-
-
-*/
